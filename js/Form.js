24fc2d1c0d81432bbd7e99d706eceab3ef8b9e4d
ushnish399 <nilanjanchakraborty5@gmail.com>
2020-01-11 14:40:40 +0530

    very short class:)

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset=createButton('reset');
   }
   hide(){
     this.greeting.hide();
@@ -16,6 +17,8 @@ class Form {
   display(){
     this.title.html("Car Racing Game");
     this.title.position(displayWidth/2 - 50, 0);
+
+    this.reset.position(displayWidth-100, 20);
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
@@ -31,6 +34,9 @@ class Form {
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
-
+this.reset.mousePressed(()=>{
+player.updateCount(0);
+game.update(0); 
+});
   }
 }
